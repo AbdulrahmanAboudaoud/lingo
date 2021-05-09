d81f4b3324b2cfe9554c45f2451451ad8bce0e37
Abdulrahman Aboudaoud <abdulrahmanaboudaoud@gmail.com>
2021-05-10 01:06:11 +0200

    create div en check functies gemakt

--- lingo.js
+++ lingo.js
@@ -12,3 +12,47 @@ var retry = document.getElementById("retry");
 var randomWord = Math.floor(Math.random()*words.length);
 
 var word = words[randomWord].split("");
+var copyWord = [...word];
+var userWord;
+
+var attempts = 5;
+var wordId = 1;
+
+var shownLetters = [false,false,false,false,false];
+
+createDivs(word.length);
+
+button.onclick = function(){
+	userWord = input.value.toLowerCase().split("");
+	if(input.value.length < 5){
+		alert("ongeldig invoer");
+	}
+	else if (input.value == words[randomWord] || attempts == 1){
+		gameOver();
+	}
+	else {
+		checkLetters();
+		createDivs(5);
+		attempts --
+		pText.innerHTML = "attempts left :" + attempts;
+		input.value = "";
+	}
+}
+function createDivs(qty){
+	var wordDiv = document.createElement("div");
+	wordDiv.id = "word_"+wordId;
+	results.appendChild(wordDiv);
+	for (i=0; i<qty; i++){
+		var createDiv_letter = document.createElement("div");
+		var node = document.creatTextNode(".");
+		createDiv_letter.appendChild(node)
+		createDiv_letter.id = wordId+"_letter_" +(i+1);
+		wordDiv.appendChild(createDiv_letter);
+		if (shownLetter[i] == true){
+			document.getElementById(wordId+"_letter_"+(i+1)).innerHTML = word[i];
+
+		}
+	}
+	document.getElementById(wordId+"_letter_"+1).innerHTML = word[0];
+	wordId++;
+}
