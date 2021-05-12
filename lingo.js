3a46d7bd168c3f5ad4675312a9dac898b0ca00bc
Abdulrahman Aboudaoud <abdulrahmanaboudaoud@gmail.com>
2021-05-12 13:17:27 +0200

    game over functie toegevoegd

--- lingo.js
+++ lingo.js
@@ -1,11 +1,11 @@
 var input = document.getElementById("input");
 var button = document.getElementById("button");
-var pText = document.getElementById("pText");
+var text = document.getElementById("pText");
 var results = document.getElementById("result");
 
 var gameDiv = document.getElementById("gameDiv");
 var gameDiv2 = document.getElementById("gameDiv2");
-var h1Text = document.getElementById("h1Text");
+var text2 = document.getElementById("text2");
 
 var retry = document.getElementById("retry");
 
@@ -34,7 +34,7 @@ button.onclick = function(){
 		checkLetters();
 		createDivs(5);
 		attempts --
-		pText.innerHTML = "attempts left :" + attempts;
+		text.innerHTML = "attempts left :" + attempts;
 		input.value = "";
 	}
 }
@@ -44,11 +44,11 @@ function createDivs(qty){
 	results.appendChild(wordDiv);
 	for (i=0; i<qty; i++){
 		var createDiv_letter = document.createElement("div");
-		var node = document.creatTextNode(".");
+		var node = document.createTextNode(".");
 		createDiv_letter.appendChild(node)
 		createDiv_letter.id = wordId+"_letter_" +(i+1);
 		wordDiv.appendChild(createDiv_letter);
-		if (shownLetter[i] == true){
+		if (shownLetters[i] == true){
 			document.getElementById(wordId+"_letter_"+(i+1)).innerHTML = word[i];
 
 		}
@@ -69,6 +69,39 @@ function checkLetters(gameOverCheck){
 			userWord[i] = "";
 		}
 	}  
+	for (i=0; i<userWord.length; i++) {
+		if (copyWord.includes(userWord[i])) {
+			document.getElementById((wordId-1)+"_letter_"+(userWord.indexOf(userWord[i])+1)).style.backgroundColor = "#FFD700";
+			document.getElementById((wordId-1)+"_letter_"+(userWord.indexOf(userWord[i])+1)).style.borderRadius = "50%";
+			copyWord[copyWord.indexOf(userWord[i])] = "-"
+			userWord[i] = "";
+			console.log(copyWord)
+		}
 
-
+	}
+	if (gameOverCheck != true) {
+		for (i=0; i<userWord.length; i++) {
+			if (copyWord[i] == "*") {
+				shownLetters[i] = true;
+			}
+		}
+	}	
 }
+
+function gameOver() {
+	retry.style.display = "block";
+	retry.onclick = function(){
+		location.reload();
+	}
+	
+	checkLetters(true);
+	if (input.value == words[randomWord]) {
+		gameDiv2.innerHTML = "Goed gedaan, je hebt het woord geraden!"
+	}
+	else if (attempts == 1) {
+		gameDiv2.innerHTML = "Helaas, je hebt het woord niet geraden. <br> Het woord was: " + words[randomWord] + ".";
+		gameDiv2.style.fontSize = "26px";
+		gameDiv2.style.color = "red";
+	}
+}
+
